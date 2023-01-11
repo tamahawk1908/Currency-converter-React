8704049c3b51a4c7c40c3aa337a3d01316fd5fba
Michał Tamaka <tamahawk@wp.pl>
2023-01-11 19:51:57 +0100

    deleted Form and moved contents to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import Form from "./Form";
+import Input from "./Input";
+import Select from "./Select";
+import Submit from "./Submit";
+import "./App.css";
 
 function App() {
   return (
     <><h1 className="header">Podręczny kantor</h1>
-      <Form />
+      <form className="form">
+        <fieldset className="form__field">
+          <legend className="form__legend">
+            Kalkulator walut
+          </legend>
+          <div>
+            Kwota*:
+            <Input />
+          </div>
+          <div>
+            Wybierz walutę:
+            <Select />
+          </div>
+            <Submit />
+          <p>
+            Pole oznaczone * jest wymagane.
+          </p>
+          <p>
+            Kurs z dnia: 09-11-2022
+          </p>
+        </fieldset>
+      </form>
     </>
   );
 }
